2d262300b10f18160dc70e74d87f65f9cb654d9b
JamesDev292 <james.barthee@orange.fr>
2025-06-17 09:00:40 +0200

    error site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
   },
-  site: 'https://jamesdev292.github.io',
+  site: 'https://jamesdev292.github.io/my-portfolio/',
   integrations: [react()]
 });
